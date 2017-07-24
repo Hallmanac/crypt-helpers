69d60bfd47bf72870b8cd50101b57031d4a701b6
Brian Hall <brian_academyjunction@outlook.com>
2017-07-24 08:04:00 -0400

    Added test project

---
+++ src/Hallmanac.CryptoHelpers/Hallmanac.CryptoHelpers.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hallmanac.CryptoHelpers.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hallmanac.CryptoHelpers.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("abfc972b-bdae-492a-bb7a-9664f5b10d05")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Hallmanac.CryptoHelpers/Hallmanac.CryptoHelpers.Tests/SymmetricEncryptionSvcTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hallmanac.CryptoHelpers.Tests
+{
+    [TestClass]
+    public class SymmetricEncryptionSvcTests
+    {
+        private const string EncryptionKey256 = "5DBE90D461C035138A6D2ABBEB16087AC6C12A6F8B70696C486E96E617239810";
+
+        [TestMethod]
+        public void Should_Encrypt_String()
+        {
+            var textToEncrypt = "The lazy fox jumped over the brown cow";
+            var symEncrypt = new SymmetricEncryptionSvc();
+
+            var expected = "FCDBB7CF7AFCE440899AE9D2949B1110C8289EF602A290467968FFF76EABA94B_4D220909BD9B34192F288931A347B1A0109931F76D7475286CA095A7F80CBF5F42BBAFEDD27ACE52C600AC1D7E061ABE1D46EFCAB6EF75531D4F40B84558082C";
+            var result = symEncrypt.Encrypt(textToEncrypt, EncryptionKey256, AesKeySize.Size256);
+
+            Assert.IsTrue(result.IsSuccessful);
+            Assert.AreEqual(expected, result.Value);
+        }
+    }
+}
