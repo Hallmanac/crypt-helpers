5e8f3f144633508da20635c73a2fef5ede253af0
Brian Hall <brian_academyjunction@outlook.com>
2017-07-30 14:17:48 -0400

    Extracted initial interface for the entry point CryptoHelper class

--- src/Hallmanac.CryptoHelpers/Hallmanac.CryptoHelpers/CryptoHelper.cs
+++ src/Hallmanac.CryptoHelpers/Hallmanac.CryptoHelpers/CryptoHelper.cs
@@ -2,11 +2,26 @@
 
 namespace Hallmanac.CryptoHelpers
 {
-    public class CryptoHelper
+    /// <summary>
+    /// A set of classes that contain helper functions that relate to cryptography such as password hashing and symmetric encryption.
+    /// </summary>
+    public class CryptoHelper : ICryptoHelper
     {
         /// <summary>
         /// Provides strong hashing services using using the standards from RFC2898 with key stretching and multiple hashing iterations on a SHA512 algorthim. 
         /// </summary>
         public IPasswordHashingSvc PasswordHashing { get; set; }
+    }
+
+
+    /// <summary>
+    /// A set of classes that contain helper functions that relate to cryptography such as password hashing and symmetric encryption.
+    /// </summary>
+    public interface ICryptoHelper
+    {
+        /// <summary>
+        /// Provides strong hashing services using using the standards from RFC2898 with key stretching and multiple hashing iterations on a SHA512 algorthim. 
+        /// </summary>
+        IPasswordHashingSvc PasswordHashing { get; set; }
     }
 }
