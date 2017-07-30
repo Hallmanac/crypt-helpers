1bf6734044f68c3777b5a6b0682a4d41526a4d2c
Brian Hall <brian_academyjunction@outlook.com>
2017-07-30 14:03:03 -0400

    Added XML documentation comments to the interface for IPasswordHashingSvc

--- src/Hallmanac.CryptoHelpers/Hallmanac.CryptoHelpers/PasswordHashingSvc.cs
+++ src/Hallmanac.CryptoHelpers/Hallmanac.CryptoHelpers/PasswordHashingSvc.cs
@@ -8,7 +8,7 @@ using Funqy.CSharp;
 namespace Hallmanac.CryptoHelpers
 {
     /// <summary>
-    /// Class that provides strong hashing capabilities.
+    /// Provides strong hashing services using using the standards from RFC2898 with key stretching and multiple hashing iterations on a SHA512 algorthim.
     /// </summary>
     public class PasswordHashingSvc : IPasswordHashingSvc
     {
@@ -22,6 +22,7 @@ namespace Hallmanac.CryptoHelpers
         private const int KeyLength = 64;
 
         private readonly string _globalApplicationSalt;
+
 
         /// <summary>
         /// Creates a new instance of the PasswordEncryptionSvc. If the application has a global application salt, then pass 
@@ -53,34 +54,33 @@ namespace Hallmanac.CryptoHelpers
         /// Computes a hash for a given password and returns the string representation for that
         /// </summary>
         /// <param name="givenPassword"></param>
-        /// <returns></returns>
         public FunqResult<PasswordHashingData> HashPassword(string givenPassword)
         {
             if (string.IsNullOrWhiteSpace(givenPassword))
             {
-                return FunqFactory.Fail("The given password was null or empty", (PasswordHashingData)null);
+                return FunqFactory.Fail("The given password was null or empty", (PasswordHashingData) null);
             }
-        
+
             var hashData = new PasswordHashingData();
             var rand = new Random();
-        
+
             // Set the hash data
             hashData.NumberOfIterations = rand.Next(MinIterationRange, MaxIterationRange);
             hashData.SaltSize = rand.Next(MinSaltSize, MaxSaltSize);
             var saltByteArray = GetRandomSalt(hashData.SaltSize);
             hashData.Salt = saltByteArray.ToHexString();
-        
+
             // Run initial hash at an application level
             var appHashedPassword = GetAppLevelPasswordHash(givenPassword);
-        
+
             // Take the output of the inital hashing and run it through proper hashing with key stretching
-            var hashedPasswordResult = 
+            var hashedPasswordResult =
                 ComputePasswordAndSaltBytes(saltByteArray, appHashedPassword, hashData.NumberOfIterations)
-                .Then(computedBytes =>
-                {
-                    var hashedPassword = computedBytes.ToHexString();
-                    return FunqFactory.Ok<string>(hashedPassword);
-                });
+                    .Then(computedBytes =>
+                    {
+                        var hashedPassword = computedBytes.ToHexString();
+                        return FunqFactory.Ok<string>(hashedPassword);
+                    });
             if (hashedPasswordResult.IsFailure)
             {
                 return FunqFactory.Fail(hashedPasswordResult.Message, hashData);
@@ -102,26 +102,28 @@ namespace Hallmanac.CryptoHelpers
         {
             if (string.IsNullOrWhiteSpace(password))
             {
-                return FunqFactory.Fail("Password was null", (byte[])null);
+                return FunqFactory.Fail("Password was null", (byte[]) null);
             }
             if (salt == null || salt.Length < 1)
             {
-                return FunqFactory.Fail("The salt did not meet the minimum length", (byte[])null);
+                return FunqFactory.Fail("The salt did not meet the minimum length", (byte[]) null);
             }
             if (salt.Length > MaxSaltSize)
             {
-                return FunqFactory.Fail("The salt length was greater than the maximum allowed", (byte[])null);
+                return FunqFactory.Fail("The salt length was greater than the maximum allowed", (byte[]) null);
             }
 
             var convertedPassword = password.ToUtf8Bytes();
             if (convertedPassword.Length > MaxPasswordSize)
             {
-                return FunqFactory.Fail($"The password length was greater than the maximum allowed. Please make it less than {MaxPasswordSize}", (byte[])null);
+                return FunqFactory.Fail($"The password length was greater than the maximum allowed. Please make it less than {MaxPasswordSize}",
+                                        (byte[]) null);
             }
 
             if (convertedPassword.Length < MinPasswordSize)
             {
-                return FunqFactory.Fail($"The password length was less than the minimum allowed. Please make it greater than {MinPasswordSize}", (byte[])null);
+                return FunqFactory.Fail($"The password length was less than the minimum allowed. Please make it greater than {MinPasswordSize}",
+                                        (byte[]) null);
             }
 
             try
@@ -148,7 +150,6 @@ namespace Hallmanac.CryptoHelpers
         /// Generates a random byte array with a length that is equivalent to the given "saltLength" parameter.
         /// </summary>
         /// <param name="saltLength">The length of the salt to be generated.</param>
-        /// <returns></returns>
         public byte[] GetRandomSalt(int saltLength)
         {
             var salt = new byte[saltLength];
@@ -162,14 +163,13 @@ namespace Hallmanac.CryptoHelpers
         /// is returned simply hashed without the global app salt.
         /// </summary>
         /// <param name="givenPassword"></param>
-        /// <returns></returns>
         public string GetAppLevelPasswordHash(string givenPassword)
         {
             if (string.IsNullOrWhiteSpace(givenPassword))
             {
                 return givenPassword;
             }
-        
+
             if (string.IsNullOrWhiteSpace(_globalApplicationSalt))
             {
                 var hash512Password = ComputeSha512ToHexString(givenPassword);
@@ -202,7 +202,7 @@ namespace Hallmanac.CryptoHelpers
             }
 
             var saltByteArray = hashData.Salt.ToHexBytes();
-        
+
             // Run initial hash at an application level
             var appHashedPassword = GetAppLevelPasswordHash(givenPassword);
 
@@ -236,7 +236,8 @@ namespace Hallmanac.CryptoHelpers
             var rng = RandomNumberGenerator.Create();
             rng.GetBytes(buffer);
         }
-        
+
+
         /// <summary>
         /// Computes a hash based on the HMACSHA512 algorithm using the given key.
         /// </summary>
@@ -252,15 +253,66 @@ namespace Hallmanac.CryptoHelpers
     }
 
 
+    /// <summary>
+    /// Provides strong hashing services using using the standards from RFC2898 with key stretching and multiple hashing iterations on a SHA512 algorthim.
+    /// </summary>
     public interface IPasswordHashingSvc
     {
+        /// <summary>
+        /// Compares a given (plain text) password with the (already hashed) password that is inside the hashData object.
+        /// </summary>
+        /// <param name="givenPassword">Plain text password to compare with</param>
+        /// <param name="hashData">
+        /// The <see cref="PasswordHashingData"/> object that contains salt size, current hashed password, etc. for use in the comparison
+        /// of the two passwords
+        /// </param>
         FunqResult ComparePasswords(string givenPassword, PasswordHashingData hashData);
+
+        /// <summary>
+        /// This will compute a hash for the given password and salt using the iterationCount parameter to determine how many times the
+        /// hashing will occur on the password + salt.
+        /// </summary>
+        /// <param name="salt">The salt that is added on to the end of the password</param>
+        /// <param name="password">The password to be hashed</param>
+        /// <param name="iterationCount">The number of times the password+salt will have a hash computed</param>
+        /// <returns></returns>
         FunqResult<byte[]> ComputePasswordAndSaltBytes(byte[] salt, string password, int iterationCount = 8000);
+
+        /// <summary>
+        /// Computes a hash based on the HMACSHA512 algorithm using the given key.
+        /// </summary>
         string ComputeSha512ToHexString(string textToHash);
+
+        /// <summary>
+        /// Generates a random byte array key based on the byte length given and returns it as a hexadecimal string.
+        /// </summary>
+        /// <param name="byteLength">Length of Byte array used in the random generator</param>
+        /// <returns>Hexadecimal text representation of the randomly generated bytes.</returns>
         string GenerateHexKeyFromByteLength(int byteLength);
+
+        /// <summary>
+        /// Generates random, non-zero bytes using the RandomNumberGenerator
+        /// </summary>
+        /// <param name="buffer">Length of random bytes to be generated.</param>
         void GenerateRandomBytes(byte[] buffer);
+
+        /// <summary>
+        /// Hashes the given plain-text password using the global application hash. If there is no global application hash then the given password 
+        /// is returned simply hashed without the global app salt.
+        /// </summary>
+        /// <param name="givenPassword"></param>
         string GetAppLevelPasswordHash(string givenPassword);
+
+        /// <summary>
+        /// Generates a random byte array with a length that is equivalent to the given "saltLength" parameter.
+        /// </summary>
+        /// <param name="saltLength">The length of the salt to be generated.</param>
         byte[] GetRandomSalt(int saltLength);
+
+        /// <summary>
+        /// Computes a hash for a given password and returns the string representation for that
+        /// </summary>
+        /// <param name="givenPassword"></param>
         FunqResult<PasswordHashingData> HashPassword(string givenPassword);
     }
 }
