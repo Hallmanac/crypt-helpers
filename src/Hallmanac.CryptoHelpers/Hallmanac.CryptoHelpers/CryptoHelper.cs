d453129e856f9dfa6bafb65df4a2213980dc695b
Brian Hall <brian_academyjunction@outlook.com>
2017-07-30 14:11:46 -0400

    Added XML doc comment on the Password Hashing property

--- src/Hallmanac.CryptoHelpers/Hallmanac.CryptoHelpers/CryptoHelper.cs
+++ src/Hallmanac.CryptoHelpers/Hallmanac.CryptoHelpers/CryptoHelper.cs
@@ -4,6 +4,9 @@ namespace Hallmanac.CryptoHelpers
 {
     public class CryptoHelper
     {
+        /// <summary>
+        /// Provides strong hashing services using using the standards from RFC2898 with key stretching and multiple hashing iterations on a SHA512 algorthim. 
+        /// </summary>
         public IPasswordHashingSvc PasswordHashing { get; set; }
     }
 }
