b3e0286a07b18c0fbef7739a06bad0314f499e33
Brian Hall <brian_academyjunction@outlook.com>
2017-07-31 07:58:09 -0400

    Added a couple of initial tests for the CryptoHelpers class so we could get some tests that exercise the password hashing

---
+++ src/Hallmanac.CryptoHelpers/Hallmanac.CryptoHelpers_Net45.Tests/CryptoHelpersTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Hallmanac.CryptoHelpers_Net45.Tests
+{
+    [TestClass]
+    public class CryptoHelpersTests
+    {
+        [TestMethod]
+        public void Should_Properly_Hash_Password()
+        {
+            var crypto = new CryptoHelpers();
+            const string password = "This is my Password!! 1234";
+            var hashedPasswordResult = crypto.PasswordHashing.HashPassword(password);
+
+            Assert.IsTrue(hashedPasswordResult.IsSuccessful);
+            Assert.IsTrue(!string.IsNullOrWhiteSpace(hashedPasswordResult.Value.HashedPassword));
+        }
+
+
+        [TestMethod]
+        public void Comparing_Passwords_Should_Result_In_Equality()
+        {
+            var crypto = new CryptoHelpers();
+            const string originalPassword = "This is my Password!! 1234";
+            var hashedPasswordResult = crypto.PasswordHashing.HashPassword(originalPassword);
+
+            const string comparePassword = "This is my Password!! 1234";
+            var comparePasswordResult = crypto.PasswordHashing.ComparePasswords(comparePassword, hashedPasswordResult.Value);
+
+            Assert.IsTrue(comparePasswordResult.IsSuccessful);
+        }
+    }
+}
