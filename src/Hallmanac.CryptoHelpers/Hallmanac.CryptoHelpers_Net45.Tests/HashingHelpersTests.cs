408be9a6376350bbedf3f592ee09837ab70ea159
Brian Hall <brian_academyjunction@outlook.com>
2017-08-02 21:44:04 -0400

    Initial adding of tests for hashing helpers

---
+++ src/Hallmanac.CryptoHelpers/Hallmanac.CryptoHelpers_Net45.Tests/HashingHelpersTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Hallmanac.CryptoHelpers_Net45.Tests
+{
+    [TestClass]
+    public class HashingHelpersTests
+    {
+        private const string TextToHash = "The Cow jumped over the moon in 5 tries while cursing $%*(@ up a storm";
+
+        [TestMethod]
+        public void Should_Create_MD5_Hash_For_Value()
+        {
+            var hh = new HashingHelpers();
+            var hashed = hh.ComputeMD5Hash(TextToHash);
+
+            Assert.IsTrue(!string.IsNullOrWhiteSpace(hashed));
+        }
+    }
+}
