2a827c6e165adc96a56cc891ce995d465217c5a6
Brian Hall <brian_academyjunction@outlook.com>
2017-08-05 10:12:37 -0400

    Added more tests for the HashingHelpers class

--- src/Hallmanac.CryptoHelpers/Hallmanac.CryptoHelpers_Net45.Tests/HashingHelpersTests.cs
+++ src/Hallmanac.CryptoHelpers/Hallmanac.CryptoHelpers_Net45.Tests/HashingHelpersTests.cs
@@ -15,6 +15,51 @@ namespace Hallmanac.CryptoHelpers_Net45.Tests
             var hashed = hh.ComputeMD5Hash(TextToHash);
 
             Assert.IsTrue(!string.IsNullOrWhiteSpace(hashed));
+            Assert.AreEqual(hashed.Length, 32);
+        }
+
+
+        [TestMethod]
+        public void ComputeSha1Hash_Should_Return_Sha1_String()
+        {
+            var hh = new HashingHelpers();
+            var hashed = hh.ComputeSha1Hash(TextToHash);
+
+            Assert.IsTrue(!string.IsNullOrWhiteSpace(hashed));
+            Assert.AreEqual(hashed.Length, 40);
+        }
+
+
+        [TestMethod]
+        public void ComputeSha256Hash_Should_Return_Sha256_String()
+        {
+            var hh = new HashingHelpers();
+            var hashed = hh.ComputeSha256Hash(TextToHash);
+
+            Assert.IsTrue(!string.IsNullOrWhiteSpace(hashed));
+            Assert.AreEqual(hashed.Length, 64);
+        }
+
+
+        [TestMethod]
+        public void ComputeSha384Hash_Should_Return_Sha384_String()
+        {
+            var hh = new HashingHelpers();
+            var hashed = hh.ComputeSha384Hash(TextToHash);
+
+            Assert.IsTrue(!string.IsNullOrWhiteSpace(hashed));
+            Assert.AreEqual(hashed.Length, 96);
+        }
+
+
+        [TestMethod]
+        public void ComputeSha512Hash_Should_Return_Sha512_String()
+        {
+            var hh = new HashingHelpers();
+            var hashed = hh.ComputeSha512Hash(TextToHash);
+
+            Assert.IsTrue(!string.IsNullOrWhiteSpace(hashed));
+            Assert.AreEqual(hashed.Length, 128);
         }
     }
 }
